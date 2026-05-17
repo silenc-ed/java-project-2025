7e22d3bdb03e6dbbf789a21f88649a7cf654d418
thanhngan-oke <su533449@gmail.com>
2026-05-17 17:42:11 +0700

    delete company

--- com.mycompany_demoproject_jar_1.0-SNAPSHOT/src/main/java/View/Customers/ProductPanel/ProductFilterListener.java
+++
@@ -1,8 +0,0 @@
-package View.Customers.ProductPanel;
-
-import Model.SanPham;
-import java.util.List;
-
-public interface ProductFilterListener {
-    void onProductsFiltered(List<SanPham> products, String sectionTitle);
-}
